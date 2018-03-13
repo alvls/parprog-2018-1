a50ba3d1f85af8ac5fb61264657aae8e77e45104
VikaSannikova <vikasannikova1998@gmail.com>
2018-03-13 22:40:45 +0300

    solution done

--- groups/1506-3/sannikova_vo/1-test-version/solution.cpp
+++ groups/1506-3/sannikova_vo/1-test-version/solution.cpp
@@ -6,49 +6,109 @@
 #include <iostream>
 #include <random>
 #include "math.h"
+#define MAXSTACK 2048
 using namespace std;
 
-//void q_Sort(vector <double> arr)
-//{
-//	int first = 0;
-//	int last = arr.size()- 1;
-//	int elem = arr[arr.size() / 2];
-//
-//	do {
-//		while (arr[first] < elem) 
-//			first++;
-//		while (arr[last] > elem) 
-//			last--;
-//
-//		if (first <= last) {
-//			int tmp = arr[first];
-//			arr[first] = arr[last];
-//			arr[last] = tmp;
-//			first++;
-//			last--;
-//		}
-//	} while (first <= last);
-//
-//	cout << "*" << endl;
-//	if (last > 0) {
-//		vector<double>vec1(arr.begin(), arr.begin() + last+1);
-//		q_Sort(vec1);
-//	}
-//	cout << "@" << endl;
-//	if (first < arr.size())
-//	{
-//		vector<double>vec2(arr.begin()+first, arr.end());
-//		q_Sort(vec2);
-//	}
-//	for (double d : arr) {
-//		cout << d << " ";
-//	}
-//	cout << endl;
-//}
+//vector <double> vec = { 6,4,1,9,3 };
 
 
+void qSort(vector<double>::iterator begin, vector<double>::iterator end)
+{
+	long i, j; // указатели, участвующие в разделении
+	long lb, ub; // границы сортируемого в цикле фрагмента
 
+	long lbstack[MAXSTACK], ubstack[MAXSTACK]; // стек запросов
+											   // каждый запрос задаетс€ парой значений,
+											   // а именно: левой(lbstack) и правой(ubstack)
+											   // границами промежутка
+	long stackpos = 1; // текуща€ позици€ стека
+	long ppos; // середина массива
 
+	double pivot; // опорный элемент
+	//vector<double>::iterator temp;
+	double temp;
+
+	lbstack[1] = 0;
+	ubstack[1] = distance(begin, end)-1;
+
+	do {
+		// ¬з€ть границы lb и ub текущего массива из стека.
+		lb = lbstack[stackpos];
+		ub = ubstack[stackpos];
+		stackpos--;
+
+		do {
+			// Ўаг 1. –азделение по элементу pivot
+			ppos = (lb + ub) >> 1; //середина 
+			i = lb; 
+			j = ub; 
+			pivot = *(begin + ppos); // vec[ppos];
+			do {
+				while (/*a[i]*/ *(begin+i) < pivot) 
+					i++;
+				while (pivot < /*a[j]*/ *(begin+j)) 
+					j--;
+				if (i <= j) {
+
+					//temp = *(begin + i); // a[i];
+					//*(begin + i) = *(begin + j); //a[i] = a[j]; 
+					//*(begin + j) = temp; //a[j] = temp;
+
+					temp = *(begin + i); // a[i];
+					*(begin + i) = *(begin + j); //a[i] = a[j]; 
+					*(begin + j) = temp; //a[j] = temp;
+
+					/*int tmp;
+					tmp = *ptra;
+					ptra = &(*ptrb);
+					ptrb = &tmp;*/
+
+					//temp = *(begin + i);
+					//begin + i = &(*(begin + j));
+					//begin + j = &temp;
+
+					i++; 
+					j--;
+				}
+			} while (i <= j);
+
+			// —ейчас указатель i указывает на начало правого подмассива,
+			// j - на конец левого (см. иллюстрацию выше), lb ? j ? i ? ub.
+			// ¬озможен случай, когда указатель i или j выходит за границу массива
+
+			// Ўаги 2, 3. ќтправл€ем большую часть в стек и двигаем lb,ub
+			if (i < ppos) { // права€ часть больше
+				if (i < ub) { // если в ней больше 1 элемента - нужно
+					stackpos++; // сортировать, запрос в стек ?????????????????????????????????????????????
+					lbstack[stackpos] = i;
+					ubstack[stackpos] = ub;
+				}
+				ub = j; // следующа€ итераци€ разделени€
+						// будет работать с левой частью
+			}
+			else { // лева€ часть больше
+				if (j > lb) {
+					stackpos++;
+					lbstack[stackpos] = lb;
+					ubstack[stackpos] = j;
+				}
+				lb = i;
+			}
+		} while (lb < ub); // пока в меньшей части более 1 элемента
+	} while (stackpos != 0); // пока есть запросы в стеке
+
+	//for (int i = 0; i < distance(begin, end);i++) {
+	//	//vec[i] = *(begin + i);
+	//	cout << *(begin + i) << " ";
+	//	//cout << vec[i] <<" ";
+	//}
+}
+
+void qSort(vector <double>* vec) {
+	vector<double>::iterator begin = vec->begin();
+	vector<double>::iterator end = vec->end();
+	qSort(begin, end);
+}
 
 
 void qSort(double* arr, int size)
@@ -88,6 +148,38 @@ void printArray(double* arr, int size) {
 
 void main()
 {
+	//int a = 0;
+	//int* ptra = &a;
+	//int b = 1;
+	//int* ptrb = &b;
+	//int* ptrtmp;
+	//cout << a << " " << b << endl;
+	//cout << ptra << " " << ptrb << endl;
+
+	//int tmp;
+	//tmp = *ptra;
+	//ptra = &(*ptrb);
+	//ptrb = &tmp;
+
+	//cout << a << " " << b << endl;
+	//cout << ptra << " " << ptrb << endl;
+
+	//ptrtmp = ptra;
+	//ptra = ptrb;
+	//ptrb = ptrtmp;
+	//cout << a << " " << b << endl;
+	//cout << ptra << " " << ptrb << endl;
+
+	////int tmp;
+	//tmp = *ptra;
+	//*ptra = *ptrb;
+	//*ptrb = tmp;
+	//cout << a << " " << b << endl;
+	//cout << ptra << " " << ptrb << endl;
+
+
+	cout << "  array" << endl;
+
 	double* arr = new double[5];
 	arr[0] = 6;
 	arr[1] = 4;
@@ -97,25 +189,21 @@ void main()
 	printArray(arr, 5);
 	qSort(arr, 5);
 	printArray(arr, 5);
+	delete[] arr;
 
+	cout << "  vector" <<endl;
 
-
-
-
-	/*cout << " hello" <<endl;
 	vector <double> vec = { 6,4,1,9,3 };
+	vector <double>* ptr = &vec;
 	for (int i = 0; i < vec.size(); i++) {
 		cout << vec[i] << " ";
 	}
 	cout << endl;
-	cout << "qq";
-	q_Sort(vec);
-	cout << "rere";
+	qSort(ptr);
 	for (int i = 0; i < vec.size(); i++) {
 		cout << vec[i] << " ";
 	}
-	cout << endl;
-	cout << "@@@@@@@@@@@@@@@@" << endl;*/
+
 	//delete[]vec;
 	system("pause>>void");
 }
