ffb427c9646e519c39fe2824921199a34d28a84e
Куликова Светлана Александровна <37101627+SylaiseElvenan@users.noreply.github.com>
2018-03-13 10:46:02 +0300

    Исключения
    Вынесла проверку на исключения из main, и добавила проверку матрицы на положительно-определенность по Критерию Сильвестра.

--- groups/1506-3/kulikova_sa/1-test-version/before_code.cpp
+++ groups/1506-3/kulikova_sa/1-test-version/before_code.cpp
@@ -4,6 +4,86 @@
 
 void NonlinearConjugateGradient(Sole * S);
 
+//Получение матрицы без i-й строки и j-го столбца
+void GetMatr(double ** mas, double ** p, int i, int m) {
+	int di = 0;
+	for (int ki = 0; ki < m - 1; ki++) { //Проверка индекса строки
+		if (ki == i) 
+			di = 1;
+		int dj = 0;
+		for (int kj = 0; kj < m - 1; kj++) { //Проверка индекса столбца
+			if (kj == 0) 
+				dj = 1;
+			p[ki][kj] = mas[ki + di][kj + dj];
+		}
+	}
+}
+
+//Рекурсивное вычисление определителя
+double Determinant(double ** mas, int m) {
+	double ** p = new double*[m];
+	for (int i = 0; i < m; i++)
+		p[i] = new double[m];
+	double d = 0;
+	int k = 1; //(-1) в степени i
+
+	if (m == 1) {
+		for (int i = 0; i < m; i++)
+			delete[] p[i];
+		delete[] p;
+
+		return(mas[0][0]);
+	}
+	if (m == 2) {
+		for (int i = 0; i < m; i++)
+			delete[] p[i];
+		delete[] p;
+
+		return(mas[0][0] * mas[1][1] - mas[1][0] * mas[0][1]);
+	}
+	for (int i = 0; i < m; i++) {
+		GetMatr(mas, p, i, m);
+		d = d + k * mas[i][0] * Determinant(p, m - 1);
+		k = -k;
+	}
+
+	for (int i = 0; i < m; i++)
+		delete[] p[i];
+	delete[] p;
+
+	return(d);
+}
+
+//Размерность системы должна быть положительной, т.е N > 0
+bool dimension(int N) {
+	if (N < 1) {
+		std::cout << "Не положительный размер системы" << std::endl;
+		return true;
+	}
+	return false;
+}
+
+//Матрица А должна быть симетричной, т.е А=Ат
+bool symmetry(double ** A, int N) {
+	for (int i = 0; i < N - 1; i++)
+		for (int j = i + 1; j < N; j++)
+			if (A[i][j] != A[j][i]) {
+				std::cout << "Матрица не симметрична" << std::endl;
+				return true;
+			}
+	return false;
+}
+
+//Матрица А должна быть положительно определена, т.е xт*А*x > 0
+bool PositiveDefinite(double ** A, int N) {
+	for (int i = 1; i <= N; i++)
+		if (Determinant(A, i) <= 0) {//Критерий Сильвестра
+			std::cout << "Матрица не положительно определена" << std::endl;
+			return true;
+		}
+	return false;
+}
+
 //argv[1] - количество потоков, argv[2] - имя входного файла, argv[3] - имя выходного файла
 int main(int argc, char * argv[]) {
 	setlocale(LC_ALL, "Russian");//Корректное отображение Кириллицы
@@ -15,10 +95,8 @@ int main(int argc, char * argv[]) {
 
 	int num_threads = atoi(argv[1]);//Количество потоков
 	int N;//Размер системы
-
 	FILE * in, * out;
 	in = fopen(argv[2], "rb");
-	out = fopen(argv[3], "wb");
 
 	fread(&N, sizeof(N), 1, in);
 
@@ -29,24 +107,16 @@ int main(int argc, char * argv[]) {
 		fread(S->A[i], sizeof(**S->A), N, in);
 	fread(S->b, sizeof(*S->b), N, in);
 
-	//Размерность системы должна быть положительной, т.е N > 0
-	if (S->N < 1) {
-		std::cout << "Не положительный размер системы" << std::endl;
-		return 2;
-	}
-	//Матрица А должна быть симетричной, т.е А=Ат
-	for (int i = 0; i < S->N - 1; i++)
-		for (int j = i + 1; j < S->N; j++)
-			if (S->A[i][j] != S->A[j][i]) {
-				std::cout << "Матрица не симметрична" << std::endl;
-				return 3;
-			}
-	//Матрица А должна быть положительно определена, т.е xт*А*x > 0
-	if (false) {//КАК??? Критерий Сильвестра???
-		std::cout << "Матрица не положительно определена" << std::endl;
-		return 4;
-	}
+	fclose(in);
 
+	if (dimension(S->N))
+		return 2;
+	if (symmetry(S->A, S->N))
+		return 3;
+	if (PositiveDefinite(S->A, S->N))
+		return 4;
+
+	out = fopen(argv[3], "wb");
 	omp_set_num_threads(num_threads);
 
 	double time = omp_get_wtime();
@@ -60,6 +130,8 @@ int main(int argc, char * argv[]) {
 		fwrite(S->A[i], sizeof(**S->A), N, out);
 	fwrite(S->b, sizeof(*S->b), N, out);
 	fwrite(S->x, sizeof(*S->x), N, out);
+
+	fclose(out);
 
 	return 0;
 }
--- groups/1506-3/kulikova_sa/1-test-version/sol.cpp
+++ groups/1506-3/kulikova_sa/1-test-version/sol.cpp
@@ -14,45 +14,45 @@ void NonlinearConjugateGradient(Sole * S) {
 	size = S->N;//Задание размера
 
 	//Выделение памяти
-	double * x = new double[S->N];//Вектор приближений
-	double * d = new double[S->N];//Вектор направления
-	double * g = new double[S->N];//Градиент
+	double * x = new double[size];//Вектор приближений
+	double * d = new double[size];//Вектор направления
+	double * g = new double[size];//Градиент
 	double s;//Скалярный шаг, величина смещения по заданному направлению
 	double coef;//Коэффициент во 2 шаге
-	double * s_prev = new double[S->N];//Произведение вектора направления и матрицы
-	double * x_prev = new double[S->N];//Предыдущее приближение
-	double * d_prev = new double[S->N];//Предыдущий вектор направления
-	double * g_prev = new double[S->N];//Предыдущий градиент
+	double * s_prev = new double[size];//Произведение вектора направления и матрицы
+	double * x_prev = new double[size];//Предыдущее приближение
+	double * d_prev = new double[size];//Предыдущий вектор направления
+	double * g_prev = new double[size];//Предыдущий градиент
  
 	//Перед выполнением первой итерации x0 и d0 полагаются равными нулю, а для вектора g0 устанавливается значение -b
-	for (int i = 0; i < S->N; i++) {
+	for (int i = 0; i < size; i++) {
 		x_prev[i] = 0;
 		d_prev[i] = 0;
 		g_prev[i] = S->b[i] * (-1);
 	}
 
 	//Цикл по количеству итераций
-	for (int i = 0; i < S->N; i++) {
+	for (int i = 0; i < size; i++) {
 		//1. Вычисление градиента
-		for (int j = 0; j < S->N; j++)
+		for (int j = 0; j < size; j++)
 			g[j] = vec(S->A[j], x_prev) - S->b[j];
 
 		//2. Вычисление вектора направления
 		coef = vec(g, g) / vec(g_prev, g_prev);
-		for (int j = 0; j < S->N; j++)
+		for (int j = 0; j < size; j++)
 			d[j] = coef * d_prev[j] - g[j];
 
 		//3. Вычисление величины смещения по заданному направлению
-		for (int j = 0; j < S->N; j++)
+		for (int j = 0; j < size; j++)
 			s_prev[j] = vec(S->A[j], d);
 		s = vec(d, g) / vec(s_prev, d);
 
 		//4. Вычисление нового приближения
-		for (int j = 0; j < S->N; j++)
+		for (int j = 0; j < size; j++)
 			x[j] = x_prev[j] - s * d[j];
 
 		//Замена текущих на предыдущие
-		for (int j = 0; j < S->N; j++) {
+		for (int j = 0; j < size; j++) {
 			g_prev[j] = g[j];
 			d_prev[j] = d[j];
 			x_prev[j] = x[j];
@@ -60,7 +60,7 @@ void NonlinearConjugateGradient(Sole * S) {
 	}
 
 	//Сохранение результата
-	for (int i = 0; i < S->N; i++)
+	for (int i = 0; i < size; i++)
 		S->x[i] = x[i];
 
 	//Освобождение памяти
