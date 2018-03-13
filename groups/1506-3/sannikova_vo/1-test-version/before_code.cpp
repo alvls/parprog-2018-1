686ebd862a6ac44919fa8a2b751dead44df0a5d3
VikaSannikova <vikasannikova1998@gmail.com>
2018-03-13 22:47:33 +0300

    add gen bc

--- groups/1506-3/sannikova_vo/1-test-version/before_code.cpp
+++ groups/1506-3/sannikova_vo/1-test-version/before_code.cpp
@@ -1,32 +1,32 @@
-//#include <cstdio>
-//#include <omp.h>
-//#include <random>
-//void qSort(double * arr, int size);
-//int main(int argc, char * argv[])
-//{
-//	int num_threads = 1;
-//	if (argc > 1)
-//		num_threads = atoi(argv[1]);
-//	int size;
-//	double *arr;
-//
-//	freopen("arr.in", "rb", stdin);
-//	freopen("arr.out", "wb", stdout);
-//
-//	fread(&size, sizeof(size), 1, stdin);
-//
-//	arr = new double[size];
-//	fread(arr, sizeof(*arr), size, stdin);
-//
-//	//omp_set_num_threads(num_threads);
-//	//double time = omp_get_wtime();
-//	qSort(arr, size);
-//
-//	//time = omp_get_wtime() - time;
-//	//fwrite(&time, sizeof(time), 1, stdout);
-//	
-//	fwrite(&size, sizeof(size), 1, stdout); // what
-//	fwrite(arr, sizeof(*arr), size, stdout);
-//	return 0;
-//}
-//#define main main_lumos_ 
+#include <cstdio>
+#include <omp.h>
+#include <random>
+void qSort(double * arr, int size);
+int main(int argc, char * argv[])
+{
+	int num_threads = 1;
+	if (argc > 1)
+		num_threads = atoi(argv[1]);
+	int size;
+	double *arr;
+
+	freopen("arr.in", "rb", stdin);
+	freopen("arr.out", "wb", stdout);
+
+	fread(&size, sizeof(size), 1, stdin);
+
+	arr = new double[size];
+	fread(arr, sizeof(*arr), size, stdin);
+
+	//omp_set_num_threads(num_threads);
+	//double time = omp_get_wtime();
+	qSort(arr, size);
+
+	//time = omp_get_wtime() - time;
+	//fwrite(&time, sizeof(time), 1, stdout);
+	
+	fwrite(&size, sizeof(size), 1, stdout); // what
+	fwrite(arr, sizeof(*arr), size, stdout);
+	return 0;
+}
+#define main main_lumos_ 
