65fb8bac5f6edc2840f64458f0ee609e343f7147
Boris Okunev <hait.08@mail.ru>
2018-03-14 17:42:37 +0300

    Added solution files

---
+++ groups/1506-3/okunev_bp/1-test-version/CRSmatrix.h
@@ -0,0 +1,154 @@
+#ifndef CRSMATRIX_H
+#define CRSMATRIX_H
+
+#include <vector>
+#include <iterator>
+
+using std::vector;
+
+class CRSmatrix {
+public:
+	CRSmatrix() : Size(0), NotZero(0) {}
+	CRSmatrix(int size, int nzelem, vector<double> value, vector<int> col, vector<int> sparseRow) : 
+		Size(size), NotZero(nzelem), Value(value), Col(col), SparseRow(sparseRow) {}
+
+	CRSmatrix(int size, int nzelem, double * value, int * col, int * sparseRow) : Size(size), NotZero(nzelem)
+	{
+		Value.assign(value, value + nzelem);
+		Col.assign(col, col + nzelem);
+		SparseRow.assign(sparseRow, sparseRow + size + 1);
+	}
+
+	int getSize()
+	{
+		return Size;
+	}
+
+	int getNotZero()
+	{
+		return NotZero;
+	}
+
+	double* getValue()
+	{
+		double* tmp = new double[NotZero];
+		std::copy(Value.begin(), Value.end(), stdext::checked_array_iterator<double*>(tmp, Value.size()));
+		return tmp;
+	}
+
+	int* getCol()
+	{
+		int* tmp = new int[NotZero];
+		std::copy(Col.begin(), Col.end(), stdext::checked_array_iterator<int*>(tmp, Col.size()));
+		return tmp;
+	}
+
+	int* getSparseRow()
+	{
+		int* tmp = new int[Size + 1];
+		std::copy(SparseRow.begin(), SparseRow.end(), stdext::checked_array_iterator<int*>(tmp, Size + 1));
+		return tmp;
+	}
+
+	CRSmatrix getTransposed() const
+	{
+		CRSmatrix tmpMatrix;
+
+		vector<vector<int>> v1(this->Size);
+		vector<vector<double>> v2(this->Size);
+
+		int position = 0;
+		for (int i = 0; i < Size; ++i)
+		{
+			while (position != NotZero && position < SparseRow[i + 1])
+			{
+				v1[Col[position]].emplace_back(i);
+				v2[Col[position]].emplace_back(Value[position]);
+				++position;
+			}
+		}
+
+		tmpMatrix.Size = Size;
+		tmpMatrix.NotZero = NotZero;
+
+		tmpMatrix.Value.reserve(NotZero);
+		tmpMatrix.Col.reserve(NotZero);
+		tmpMatrix.SparseRow.resize(Size + 1);
+
+		tmpMatrix.SparseRow[0] = 0;
+
+		for (int i = 1; i <= Size; ++i)
+			tmpMatrix.SparseRow[i] = tmpMatrix.SparseRow[i - 1] + v1[i - 1].size();
+
+		for (int i = 0; i < Size; ++i)
+		{
+			tmpMatrix.Col.insert(tmpMatrix.Col.end(), v1[i].begin(), v1[i].end());
+			tmpMatrix.Value.insert(tmpMatrix.Value.end(), v2[i].begin(), v2[i].end());
+		}
+
+		return tmpMatrix;
+	}
+
+	CRSmatrix operator*(const CRSmatrix & crsmatrix)
+	{
+		CRSmatrix tmp = crsmatrix.getTransposed();
+		CRSmatrix tmpMatrix;
+		tmpMatrix.Size = Size;
+		tmpMatrix.SparseRow.resize(Size + 1);
+
+		int pos1 = 0;
+
+
+		int lastSize = 0;
+
+		for (int i = 0; i < Size; ++i)
+		{
+			int pos2 = 0;
+
+			for (int j = 0; j < Size; ++j)
+			{
+				pos1 = SparseRow[i];
+				pos2 = tmp.SparseRow[j];
+
+				double sum = 0;
+
+				while (pos1 < this->SparseRow[i + 1] && pos2 < tmp.SparseRow[j + 1])
+				{
+					if (this->Col[pos1] == tmp.Col[pos2])
+					{
+						sum += Value[pos1] * tmp.Value[pos2];
+						pos1++;
+						pos2++;
+					}
+					else if (this->Col[pos1] > tmp.Col[pos2])
+						pos2++;
+					else if (this->Col[pos1] < tmp.Col[pos2])
+						pos1++;
+				}
+
+				if (sum != 0)
+				{
+					tmpMatrix.Col.emplace_back(j);
+					tmpMatrix.Value.emplace_back(sum);
+				}
+
+
+			}
+			tmpMatrix.SparseRow[i] = lastSize;
+			lastSize = tmpMatrix.Value.size();
+		}
+
+		tmpMatrix.SparseRow[Size] = tmpMatrix.Col.size();
+
+		tmpMatrix.NotZero = tmpMatrix.Col.size();
+
+		return tmpMatrix;
+	}
+private:
+	int Size;
+	int NotZero;
+	vector<double> Value;
+	vector<int> Col;
+	vector<int> SparseRow;
+};
+#endif // !CRSMATRIX_H
---
+++ groups/1506-3/okunev_bp/1-test-version/sol.cpp
@@ -0,0 +1,14 @@
+#include "CRSmatrix.h"
+
+void CRSMatrixMult(int matrixSize, int notZeroA, double* valueA, int* colA, int* rowA, int notZeroB, double* valueB, int* colB, int* rowB,
+	int& notZeroC, double* valueC, int* colC, int* rowC)
+{
+	CRSmatrix matrixA(matrixSize, notZeroA, valueA, colA, rowA);
+	CRSmatrix matrixB(matrixSize, notZeroB, valueB, colB, rowB);
+	CRSmatrix matrixC = matrixA * matrixB;
+
+	notZeroC = matrixC.getNotZero();
+	valueC = matrixC.getValue();
+	colC = matrixC.getCol();
+	rowB = matrixC.getSparseRow();
+}
