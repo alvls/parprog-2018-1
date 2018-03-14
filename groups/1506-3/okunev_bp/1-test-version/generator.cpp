4718cd353abae9bec3ee4a026bd563ac5d94e336
Boris Okunev <hait.08@mail.ru>
2018-03-14 22:22:58 +0300

    Implemented generator

---
+++ groups/1506-3/okunev_bp/1-test-version/generator.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <random>
+#include <ctime>
+#include <chrono> 
+#include <vector>
+
+using std::vector;
+
+const int DEFAULT_MATRIX_SIZE = 1000;
+const double DENSITY = 0.1;
+
+int main(int argc, char* argv[])
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<double> distribution(- 1000, 1000);
+	std::uniform_int_distribution<int> distributionInt(0, 1000);
+
+	char* PATHout = "matr.out";
+	int matrixSize = DEFAULT_MATRIX_SIZE;
+	int notZero = 0;
+	int elemToRow = DEFAULT_MATRIX_SIZE * DENSITY;
+	int testType = 0;
+
+	vector<double> value;
+	vector<int> col;
+	vector<int> row(matrixSize + 1);
+	value.reserve(matrixSize * elemToRow);
+	col.reserve(matrixSize * elemToRow);
+
+	int notZeroB = 0;
+	vector<double> valueB;
+	vector<int> colB;
+	vector<int> rowB(matrixSize + 1);
+	valueB.reserve(matrixSize * elemToRow);
+	colB.reserve(matrixSize * elemToRow);
+
+	if (argc > 1)
+	{
+		PATHout = argv[1];
+		if (argc > 3)
+		{
+			matrixSize = atoi(argv[2]);
+			if (argc == 4)
+				testType = atoi(argv[3]);
+		}
+	}
+
+	freopen(PATHout, "wb", stdout);
+
+	switch (testType)
+	{
+	case 0:
+		for (int i = 0; i < matrixSize; ++i)
+		{
+			vector<int> tmp;
+			tmp.reserve(elemToRow);
+			for (int j = 0; j < elemToRow; ++j)
+			{
+				value.emplace_back(distribution(gen));
+				tmp.emplace_back(distributionInt(gen));
+			}
+			std::sort(tmp.begin(), tmp.end());
+			col.insert(col.end(), tmp.begin(), tmp.end());
+			row[i] = i * elemToRow;
+		}
+		row[matrixSize] = row[matrixSize - 1] + 1;
+		notZero = value.size();
+
+		for (int i = 0; i < matrixSize; ++i)
+		{
+			vector<int> tmp;
+			tmp.reserve(elemToRow);
+			for (int j = 0; j < elemToRow; ++j)
+			{
+				valueB.emplace_back(distribution(gen));
+				tmp.emplace_back(distributionInt(gen));
+			}
+			std::sort(tmp.begin(), tmp.end());
+			colB.insert(colB.end(), tmp.begin(), tmp.end());
+			rowB[i] = i * elemToRow;
+		}
+		rowB[matrixSize] = rowB[matrixSize - 1] + 1;
+		notZeroB = valueB.size();
+		break;
+	default:
+		break;
+	}
+	
+	fwrite(&matrixSize, sizeof(matrixSize), 1, stdout);
+	if (notZero == 0)
+		throw("Invalid test");
+	fwrite(&notZero, sizeof(notZero), 1, stdout);
+	fwrite(value.data(), sizeof(value[0]), value.size(), stdout);
+	fwrite(col.data(), sizeof(col[0]), col.size(), stdout);
+	fwrite(row.data(), sizeof(row[0]), row.size(), stdout);
+
+	fwrite(&matrixSize, sizeof(matrixSize), 1, stdout);
+	if (notZeroB == 0)
+		throw("Invalid test");
+	fwrite(&notZeroB, sizeof(notZeroB), 1, stdout);
+	fwrite(valueB.data(), sizeof(valueB[0]), valueB.size(), stdout);
+	fwrite(colB.data(), sizeof(colB[0]), colB.size(), stdout);
+	fwrite(rowB.data(), sizeof(rowB[0]), rowB.size(), stdout);
+
+	return 0;
+}
