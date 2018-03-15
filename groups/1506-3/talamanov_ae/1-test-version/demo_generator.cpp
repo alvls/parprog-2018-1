b1d106faecf56bc6bc30b0f5e877e31d8003d9a5
tolik <talamtolik@gmail.com>
2018-03-15 19:36:18 +0300

    Add generator demo
    You should set number test, size of array and out file path.

---
+++ groups/1506-3/talamanov_ae/1-test-version/demo_generator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <string>
+#include <stdexcept>
+#include "generator.h"
+
+using namespace std;
+
+void writeArrayToFile(vector<int> array, const string& file_name);
+
+int main(int argc, char* argv[]) {
+
+  if(argc != 4) {
+    cerr << "You should set :" << endl;
+    cerr << "number test = [0, 5]" << endl;
+    cerr << "array size " << endl;
+    cerr << "output file path " << endl;
+
+    throw runtime_error("invalid parameters");
+  }
+
+  auto type = static_cast<Generator::TestType>(atoi(argv[1]));
+  int size_array = atoi(argv[2]);
+  string out_file_path = string(argv[3]);
+
+  Generator generator;
+  auto array = generator.generate(type, size_array);
+
+  writeArrayToFile(array, out_file_path);
+
+  return 0;
+}
+
+void writeArrayToFile(vector<int> array, const string& file_path) {
+  ofstream file(file_path, ios::binary);
+
+  ostream_iterator<int> file_iterator(file, " ");
+  copy(array.begin(), array.end(), file_iterator);
+
+  file.close();
+}
