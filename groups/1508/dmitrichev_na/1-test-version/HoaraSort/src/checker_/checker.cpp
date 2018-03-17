417a4bab4ecce068c2c339dcf9f354d59d80498e
Nick Dmitrichev <dmitrichev.nick@gmail.com>
2018-03-17 22:18:52 +0300

    Fixed checker

--- groups/1508/dmitrichev_na/1-test-version/HoaraSort/src/checker_/checker.cpp
+++ groups/1508/dmitrichev_na/1-test-version/HoaraSort/src/checker_/checker.cpp
@@ -3,60 +3,59 @@
 
 int main (int argc, char* argv[])
 {
-   if (argc != 3) {
-      std::cout << argv[0] << " -> ERROR: not enough arguments\n";
+   if (argc != 2) {
+      cerr << argv[0] << " -> ERROR: not enough arguments\n";
       exit(1);
    }
 
-   FILE *buo = NULL,
-        *bua = NULL;
+   FILE *buo = NULL;
    if ((buo=fopen(argv[1], "rb"))==NULL){
        cerr << argv[0] << " -> ERROR: can not open '"<<argv[1]<<"'\n";
        exit(1);
    }
-   if ((bua=fopen(argv[2], "rb"))==NULL){
-       cerr << argv[0] << " -> ERROR: can not open '"<<argv[2]<<"'\n";
-       exit(1);
-   }
 
-   int N;
+   int No;
    double res_time;
 
-   fread(&res_time, sizeof (res_time), 1, bua);
    fread(&res_time, sizeof (res_time), 1, buo);
-   fread(&N, sizeof (N), 1, buo);
-   double *ans = new double[N],
-          *res = new double[N];
-   fread(res, sizeof (*res), N, buo);
-   fread(&N, sizeof (N), 1, bua);
-   fread(ans, sizeof (*ans), N, bua);
+   fread(&No, sizeof (No), 1, buo);
 
-   bool result = true;
-   for (int i = 0; i < N; ++i)
-     if (ans[i]!=res[i])
-       {
-         result=false;
-         break;
-       }
-
-   if (result)
-   {
-     checker_result.write_message ("AC. Numbers are equal.");
-     checker_result.write_verdict (verdict::AC);
+   if (No==0){
+       checker_result.write_message ("AC. Zero size array.");
+       checker_result.write_verdict (verdict::AC);
    }
-   else
-   {
-     checker_result.write_message ("WA. Output is not correct.");
-     checker_result.write_verdict (verdict::WA);
-   }
+   else {
+     double curNum, prevNum;
 
+     fread(&curNum, sizeof (curNum), 1, buo);
+     prevNum=curNum;
+
+     bool result = true;
+     for (int i = 1; i < No; ++i) {
+         fread(&curNum, sizeof (curNum), 1, buo);
+         if (curNum<prevNum)
+         {
+           result=false;
+           break;
+         }
+         prevNum=curNum;
+     }
+
+     if (result)
+     {
+       checker_result.write_message ("AC. Array is sorted.");
+       checker_result.write_verdict (verdict::AC);
+     }
+     else
+     {
+       checker_result.write_message ("WA. Array isn't sorted.");
+       checker_result.write_verdict (verdict::WA);
+     }
+   }
    checker_result.write_time (res_time * 1e7);
 
    cout << argv[0] << " -> OK\n";
 
-   fclose(bua);
    fclose(buo);
-   delete[] ans;
-   delete[] res;
    return 0;
 }
