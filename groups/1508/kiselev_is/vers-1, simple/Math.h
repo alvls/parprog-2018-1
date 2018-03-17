e573f435b941aa64790b0e1a279865953b825968
Umlore1998 <iv.kiselev1998@yandex.ru>
2018-03-17 20:06:21 +0300

    Правильно работающий код

--- groups/1508/kiselev_is/vers-1, simple/Math.h
+++ groups/1508/kiselev_is/vers-1, simple/Math.h
@@ -9,15 +9,14 @@
 		double result = 0.0;
 		double point = 0.0;
 		int size = fun->GetSize();
-		/*for (int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 				point = fun->GetCoeff(i)*(pow(Xpoint, fun->GetXindex(i))) * pow(Ypoint, fun->GetYindex(i));
 			if ((fun->GetXcos(i) != 0) || (fun->GetYcos(i) != 0))
 				point = point * cos(pow(Xpoint, fun->GetXcos(i))*pow(Ypoint, fun->GetYcos(i)));
 			if ((fun->GetXsin(i) != 0) || (fun->GetYsin(i) != 0))
 				point = point * sin(pow(Xpoint, fun->GetXsin(i))*pow(Ypoint, fun->GetYsin(i)));
 			result = result + point;
-				
-		}*/
+		}
 		return result;
 		};
 
@@ -50,7 +49,7 @@
 	}
 	*/
 
-	double TIntegral(Func* fun, double start, double finish, double accuracy, int parts = 20) {
+	double TIntegral(Func* fun, double start, double finish, double accuracy, int parts = 200) {
 
 		double result = 0.0;
 
@@ -61,15 +60,23 @@
 		double Ypart = Xpart;
 		double Xpoint = start;
 		double Ypoint = start;
+
+		double variable = 0.0;
+
 		// добавить точность
 		for (int i = 0; i < parts; i++) {
 			XHigh = ((valueIn(fun, Xpoint, Ypoint) + valueIn(fun, Xpoint + Xpart, Ypoint)) / 2);
+			
+			variable = XHigh;
 			// (F0 + F1) / 2  // определение среднего значения трапеции
 			for (int j = 0; j < parts; j++) {
 				YHigh = ((valueIn(fun, Xpoint, Ypoint + Ypart) + valueIn(fun, Xpoint ,Ypoint + Ypart)) / 2);
+				
 				// (F0 + F1) / 2  // определение среднего значения трапеции через шаг по оси Y
-				result = result + ((YHigh + XHigh) / 2 * Xpart * Ypart); // среднее между этими значениями
+				result = result + ((YHigh + variable) / 2 * Xpart * Ypart); // среднее между этими значениями
+				variable = YHigh;
 				Ypoint = Ypoint + Ypart;
+
 			}
 			Ypoint = start;
 			Xpoint = Xpoint + Xpart;
