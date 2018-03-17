495c733c31f3da89aabc1c68374925b8c756f749
Alex-Sergeev <serega-al_ss2@mail.ru>
2018-03-17 22:15:10 +0300

    Релизная версия
    Добавлены: solver, тесты для solver, generator, checker, добавлены
    14
    тестов.
    Множество багфиксов.

---
+++ groups/1506-3/Sergeev_AP/1-test-version/Grehem/Generator/generator.cpp
@@ -0,0 +1,50 @@
+﻿#include "Grehem.h"
+#include <random>
+#include <stdio.h>
+
+void genRandArray(dot* arr, int size)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(-100000, 100000);
+	for (int i = 0; i < size; i++)
+	{
+		arr[i].x = dis(gen);
+		arr[i].y = dis(gen);
+	}
+}
+
+//Запуск через командную строку с вводом  1) номер теста, 2)имя файлы входных данных 3)имя файла выходных данных
+int main(int argc, char * argv[])
+{
+	if (argc < 4)
+	{
+		std::cout << "ERROR: No file names" << std::endl;
+		return 1;
+	};
+	//Алгоритм Грехема строит выпуклую оболочку только для 3 и более точек!
+	int size_tests[] = { 3, 4, 5, 6, 8, 10, 12, 15, 20, 30, 40, 50, 100, 200, 300, 400, 500, 1000, 2000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000 };
+	FILE* file;
+	char* fileName = argv[2];
+	file = fopen(fileName, "wb");
+	int size = size_tests[atoi(argv[1])];
+
+	fwrite(&size, sizeof(size), 1, file);
+	dot* dotArr = new dot[size + 1];
+	//Генерация массива точек
+	genRandArray(dotArr, size);
+	//Запись массива
+	fwrite(dotArr, sizeof(*dotArr), size, file);
+	fclose(file);
+
+	fileName = argv[3];
+	file = fopen(fileName, "wb");
+	//Нахождение выпуклой оболочки
+	std::pair<dot*, int> answer = grehemMethod(dotArr, size + 1);
+	//Запись оболочки в файл
+	fwrite(&answer.second, sizeof(int), 1, file);
+	fwrite(answer.first, sizeof(*answer.first), answer.second, file);
+	fclose(file);
+
+	return 0;
+}
---
+++ groups/1506-3/Sergeev_AP/1-test-version/Grehem/Solver/Main.cpp
@@ -0,0 +1,43 @@
+﻿#include <random>
+#include <chrono>
+#include <iostream>
+#include "Grehem.h"
+
+void genRandArray(dot* arr, int size)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(-100000, 100000);
+	for (int i = 0; i < size; i++)
+	{
+		arr[i].x = dis(gen);
+		arr[i].y = dis(gen);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	setlocale(LC_ALL, "Russian");
+	int size = 50;
+	dot* arr = new dot[size + 1];
+	genRandArray(arr, size);
+
+	std::cout << "Сгенерированный массив" << std::endl;
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i].x << " " << arr[i].y << std::endl;
+	}
+
+	auto start = std::chrono::high_resolution_clock::now();
+	std::pair<dot*, int> answer = grehemMethod(arr, size + 1);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << " Время выполнения " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " (микросекунды)" << std::endl;
+
+	std::cout << "Выпуклая оболочка" << std::endl;
+	for (int i = 0; i < answer.second; i++)
+	{
+		std::cout << answer.first[i].x << " " << answer.first[i].y << std::endl;
+	}
+
+	return 0;
+}
