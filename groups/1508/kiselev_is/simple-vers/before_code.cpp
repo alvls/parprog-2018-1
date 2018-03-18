eb1096a0b1a0bb9112b5b6f5371054a87e7cf3c5
Umlore1998 <iv.kiselev1998@yandex.ru>
2018-03-18 12:50:14 +0300

    My quest

--- groups/1508/kiselev_is/simple-vers/before_code.cpp
+++ groups/1508/kiselev_is/simple-vers/before_code.cpp
@@ -1,27 +1,53 @@
 #include <cstdio> 
 #include <omp.h> 
 #include <random>
+#include "Func.h"
 
-void MatrMatrMult(double * A, double * B, double * C, int N);
+double TIntegral(Func* fun, double start, double finish, double accuracy, int parts = 200);
 
 int main(int argc, char * argv[]) {
 	int num_threads = 1;
 	if (argc > 1) num_threads = atoi(argv[1]);
-	int N;
-	double *A, *B, *C;
+	int size;
+
+	double integral = 0.0;
+
 	freopen("matr.in", "rb", stdin);
 	freopen("matr.out", "wb", stdout);
-	fread(&N, sizeof(N), 1, stdin);
-	A = new double[N * N];
-	B = new double[N * N];
-	C = new double[N * N];
-	fread(A, sizeof(*A), N * N, stdin);
-	fread(B, sizeof(*B), N * N, stdin);
+	fread(&size, sizeof(size), 1, stdin);
+
+	double* buf = new double(size);
+
+	double start = 0.0;
+	double finish = 4.0;
+
+	Func* fun = new Func(size);
+	for (int i = 0; i < 7; i++) {
+		fread(buf, sizeof(*buf), size, stdin);
+		switch (i) {
+		case 0: fun->SetCoeffs(buf);
+		case 1: fun->SetXindexs(buf);
+		case 2: fun->SetYindexs(buf);
+		case 3: fun->SetXcos(buf);
+		case 4: fun->SetYcos(buf);
+		case 5: fun->SetXsin(buf);
+		case 6: fun->SetYsin(buf);
+		default:
+			break;
+		}
+	}
+
+
+	fread(&start, sizeof(start), 1, stdin);
+	fread(&finish, sizeof(finish), 1, stdin);
+
 	omp_set_num_threads(num_threads);
 	double time = omp_get_wtime();
-	MatrMatrMult(A, B, C, N);
+	
+	integral = TIntegral(fun, start, finish, 0.01);
+
 	time = omp_get_wtime() - time;
 	fwrite(&time, sizeof(time), 1, stdout);
-	fwrite(C, sizeof(*C), N * N, stdout);
+	fwrite(&integral, sizeof(integral), 1, stdout);
 	return 0;
 }
--- groups/1508/kiselev_is/simple-vers/sol.cpp
+++ groups/1508/kiselev_is/simple-vers/sol.cpp
@@ -1,15 +1,81 @@
 
-#include <omp.h>
+#pragma once
 
-void MatrMatrMult(double * A, double * B, double * C, int N) {
-	int i, j, k;
-#pragma omp parallel for private(j, k) 
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			C[i * N + j] = 0.0;
-			for (k = 0; k < N; k++)
-				C[i * N + j] += A[i * N + k] * B[k * N + j];
-		}
+#include <math.h>
+#include <cmath>
+#include "Func.h"
+
+// Значение функции в точке
+double valueIn(Func* fun, double Xpoint, double Ypoint) {
+	double result = 0.0;
+	double point = 0.0;
+	int size = fun->GetSize();
+	for (int i = 0; i < size; i++) {
+		point = fun->GetCoeff(i)*(pow(Xpoint, fun->GetXindex(i))) * pow(Ypoint, fun->GetYindex(i));
+		if ((fun->GetXcos(i) != 0) || (fun->GetYcos(i) != 0))
+			point = point * cos(pow(Xpoint, fun->GetXcos(i))*pow(Ypoint, fun->GetYcos(i)));
+		if ((fun->GetXsin(i) != 0) || (fun->GetYsin(i) != 0))
+			point = point * sin(pow(Xpoint, fun->GetXsin(i))*pow(Ypoint, fun->GetYsin(i)));
+		result = result + point;
 	}
+	return result;
+};
+
+// Модуль
+double module(double a) {
+	return (a > 0) ? a : -a;
+}
+
+// Интеграл трапециями, функция, начало отрезка, конец, точность, начальное количество частей
+
+double TIntegral(Func* fun, double start, double finish, double accuracy, int parts = 200) {
+
+	double result = 0.0;
+
+	double XHigh = 0.0;
+	double YHigh = 0.0;
+
+	double Xpart = (finish - start) / parts;
+	double Ypart = Xpart;
+	double Xpoint = start;
+	double Ypoint = start;
+
+	double variable = 0.0;
+	double LastResult = valueIn(fun, (finish - start) / 2, (finish - start) / 2);
+	while (module(result - LastResult) > accuracy) {
+
+		for (int i = 0; i < parts; i++) {
+			XHigh = ((valueIn(fun, Xpoint, Ypoint) + valueIn(fun, Xpoint + Xpart, Ypoint)) / 2);
+
+			variable = XHigh;
+			// (F0 + F1) / 2  // определение среднего значения трапеции
+			for (int j = 0; j < parts; j++) {
+				YHigh = ((valueIn(fun, Xpoint, Ypoint + Ypart) + valueIn(fun, Xpoint, Ypoint + Ypart)) / 2);
+
+				// (F0 + F1) / 2  // определение среднего значения трапеции через шаг по оси Y
+				result = result + ((YHigh + variable) / 2 * Xpart * Ypart); // среднее между этими значениями
+				variable = YHigh;
+				Ypoint = Ypoint + Ypart;
+
+			}
+			Ypoint = start;
+			Xpoint = Xpoint + Xpart;
+		}
+		if (module(LastResult - result) > (accuracy / 20)) {
+			if (module(LastResult - result) > accuracy * 100) parts += 1000;
+			else if (module(LastResult - result) > accuracy * 10) parts += 200;
+			else if (module(LastResult - result) > accuracy / 10) parts += 50;
+			LastResult = result;
+			result = 0;
+			Xpart = (finish - start) / parts;
+			Ypart = Xpart;
+			Xpoint = start;
+			Ypoint = start;
+
+			std::cout << parts << std::endl;
+		}
+
+	}
+	return result;
 }
 
