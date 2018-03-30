fe76e9fb39797c09923984b37e2309158d21788c
tolik <talamtolik@gmail.com>
2018-03-31 01:02:16 +0300

    Modify scripts
    Set path to exe files, tests data and result checker file

--- groups/1506-3/talamanov_ae/1-test-version/checker/checker.py
+++ groups/1506-3/talamanov_ae/1-test-version/checker/checker.py
@@ -2,13 +2,12 @@ from subprocess import Popen, PIPE
 from sys import argv
 from os import listdir
 
-exe_path         = argv[1]
-binary_files_dir = argv[2]
-log_file_path    = argv[3]
+exe_path         = "../build/bin/checker"
+binary_files_dir = "tests/"
+log_file_path    = "checker_result.txt"
 
 def check(in_file_name):
     comand = "./" + exe_path + " " + binary_files_dir + in_file_name + " " + log_file_path 
-
     proc = Popen(comand, shell=True, stdout=PIPE, stderr=PIPE)
     proc.wait()
 
--- groups/1506-3/talamanov_ae/1-test-version/generator/generator.py
+++ groups/1506-3/talamanov_ae/1-test-version/generator/generator.py
@@ -1,15 +1,20 @@
+import os
 from subprocess import Popen, PIPE
 from sys import argv
 
-exe_path = argv[1]
-save_dir = argv[2]
+exe_path = "../build/bin/generator"
+save_dir = "tests"
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
 def generate(size_array, type_test, save_name):
-    comand = "./" + exe_path + " " + str(type_test) + " " + str(size_array) + " " + save_dir +  str(save_name)
+    comand = "./" + exe_path + " " + str(type_test) + " " + str(size_array) + " " + save_dir + "/" +  str(save_name)
+
     proc = Popen(comand, shell=True, stdout=PIPE, stderr=PIPE)
     proc.wait()
     
-sizes = [10, 100, 1000]
+sizes = [10, 100, 1000, 10000]
 
 count_type = 6;
 
