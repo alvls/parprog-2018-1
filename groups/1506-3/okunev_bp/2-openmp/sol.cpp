5cc47475288aaf1e78559d1996eafa44e0646b7c
Boris Okunev <hait.08@mail.ru>
2018-04-13 22:45:17 +0300

    add before_code file

--- groups/1506-3/okunev_bp/2-openmp/sol.cpp
+++ groups/1506-3/okunev_bp/2-openmp/sol.cpp
@@ -1,17 +1,9 @@
 #include "CRSmatrix.h"
 #include <omp.h>
 
-void CRSMatrixMult(int matrixSize, int notZeroA, double* valueA, int* colA, int* rowA, int notZeroB, double* valueB, int* colB, int* rowB,
-	int& notZeroC, double*& valueC, int*& colC, int*& rowC, int threads)
+void CRSMatrixMult(CRSmatrix &matrixA, CRSmatrix &matrixB, CRSmatrix &matrixC, int threads)
 {
 	omp_set_num_threads(threads);
-
-	CRSmatrix matrixA(matrixSize, notZeroA, valueA, colA, rowA);
-	CRSmatrix matrixB(matrixSize, notZeroB, valueB, colB, rowB);
-	CRSmatrix matrixC = matrixA * matrixB;
-
-	notZeroC = matrixC.getNotZero();
-	valueC = matrixC.getValue();
-	colC = matrixC.getCol();
-	rowC = matrixC.getSparseRow();
+	
+	matrixC = matrixA * matrixB;
 }
