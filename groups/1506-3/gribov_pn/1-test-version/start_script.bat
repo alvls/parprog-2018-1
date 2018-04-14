cf258b3b0aab729ba514e8eeac16b0ce72142f56
Pavel Gribov <gribovy2010@gmail.com>
2018-04-14 21:02:44 +0300

    Обновление скрипта запуска

--- groups/1506-3/gribov_pn/1-test-version/start_script.bat
+++ groups/1506-3/gribov_pn/1-test-version/start_script.bat
@@ -1,5 +1,6 @@
 rem скрипт запуска генератора тестов в папку (25 тестов), программы сортировки сгенерированных файлов, 
 rem программы доверительной сортировки и программы проверки (все выходные файлы ложатся в папку tests)
+mkdir tests
 FOR /L %%i IN (1,1,25) DO start /wait Generator.exe %%i
 FOR /L %%i IN (1,1,25) DO start /wait Solution.exe %%i
 FOR /L %%i IN (1,1,25) DO start /wait TrueSolution.exe %%i
