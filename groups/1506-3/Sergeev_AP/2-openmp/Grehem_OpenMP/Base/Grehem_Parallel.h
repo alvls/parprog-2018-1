b9f6111c08af519d8321e8f80268fb7d6a5fba4c
Alex-Sergeev <serega-al_ss2@mail.ru>
2018-04-14 23:55:07 +0300

    Финальная версия

--- groups/1506-3/Sergeev_AP/2-openmp/Grehem_OpenMP/Base/Grehem_Parallel.h
+++ groups/1506-3/Sergeev_AP/2-openmp/Grehem_OpenMP/Base/Grehem_Parallel.h
@@ -55,6 +55,58 @@ void merge(int firstThread, int secThread, dot* arr, std::pair<int, int>* ind_ar
 	delete[] tmparr_2;
 }
 
+void mergeForMin(int firstThread, int secThread, dot* arr, std::pair<int, int>* ind_array)
+{
+	int size_1 = ind_array[firstThread].second - ind_array[firstThread].first;
+	int size_2 = ind_array[secThread].second - ind_array[secThread].first;
+	dot* tmparr_1 = new dot[size_1];
+	for (int i = 0; i < size_1; i++)
+	{
+		tmparr_1[i] = arr[ind_array[firstThread].first + i];
+	}
+	dot* tmparr_2 = new dot[size_2];
+	for (int i = 0; i < size_2; i++)
+	{
+		tmparr_2[i] = arr[ind_array[secThread].first + i];
+	}
+
+	int iter1 = 0;
+	int iter2 = 0;
+	while ((iter1 != size_1) && (iter2 != size_2))
+	{
+		if ((tmparr_1[iter1].x < tmparr_2[iter2].x) || ((tmparr_1[iter1].x == tmparr_2[iter2].x) && (tmparr_1[iter1].y < tmparr_2[iter2].y)))
+		{
+			arr[ind_array[firstThread].first + iter1 + iter2] = tmparr_1[iter1];
+			iter1++;
+		}
+		else
+		{
+			arr[ind_array[firstThread].first + iter1 + iter2] = tmparr_2[iter2];
+			iter2++;
+		}
+	}
+	if (iter1 == size_1)
+	{
+		for (int i = iter2; i < size_2; i++)
+		{
+			arr[ind_array[firstThread].first + i + size_1] = tmparr_2[i];
+		}
+	}
+	else if (iter2 == size_2)
+	{
+		for (int i = iter1; i < size_1; i++)
+		{
+			arr[ind_array[firstThread].first + i + size_2] = tmparr_1[i];
+		}
+	}
+	ind_array[firstThread].second = ind_array[secThread].second;
+	ind_array[secThread].first = 0;
+	ind_array[secThread].second = 0;
+
+	delete[] tmparr_1;
+	delete[] tmparr_2;
+}
+
 void quickSort(dot* arr, int startInd, int endInd)
 {
 	if (startInd < endInd)
@@ -135,9 +187,48 @@ void quickSortSearchMin(dot* arr, int startInd, int endInd)
 	}
 }
 
-void searchMinElement(dot* dotArray, int size, int& num_threads)
+void searchMinElement(dot* dotArray, int size, int numThreads)
 {
-	quickSortSearchMin(dotArray, 0, size);
+	if (size < numThreads * 2) //Если размер массива меньше двойного кол-ва потоков выполняется сортировка всего массива в одном потоке
+	{
+		quickSortSearchMin(dotArray, 0, size);
+		return;
+	}
+	std::pair<int, int>* ind_array = new std::pair<int, int>[numThreads];
+	std::div_t portion = div(size, numThreads); // size / num_threads
+	ind_array[0].first = 0;
+	ind_array[0].second = portion.quot;
+	if (portion.rem > 0)
+		ind_array[0].second++;
+	for (int i = 1; i < numThreads; i++)
+	{
+		ind_array[i].first = ind_array[i - 1].second;
+		ind_array[i].second = ind_array[i].first + portion.quot;
+		if (portion.rem > i)
+			ind_array[i].second++;
+	}//закончено распределение индексов
+
+	int steps_amount = std::ceil(log2(numThreads)); // расчёт необходимого числа шагов в цикле слияний массивов 
+
+#pragma omp parallel
+	{
+		int thread_num = omp_get_thread_num();
+		quickSortSearchMin(dotArray, ind_array[thread_num].first, ind_array[thread_num].second);
+#pragma omp barrier
+		for (int i = 0; i < steps_amount; i++)
+		{
+			if (thread_num % (int)(pow(2, i + 1)) == 0) //Если поток ведущий - сделать слияние с "соседним" потоком
+			{
+				if (thread_num + (int)pow(2, i) < numThreads) // Если поток с которым происходит слияние существует
+				{
+					mergeForMin(thread_num, thread_num + (int)pow(2, i), dotArray, ind_array);// Слить массив первого потока с массивом второго
+				}
+			}
+#pragma omp barrier
+		}
+	}
+
+	delete[] ind_array;
 }
 
 std::pair<dot*, int> grehemMethod_OpenMP(dot* dotArray, int size, int numThreads)
--- groups/1506-3/Sergeev_AP/2-openmp/Grehem_OpenMP/Base/Main.cpp
+++ groups/1506-3/Sergeev_AP/2-openmp/Grehem_OpenMP/Base/Main.cpp
@@ -8,7 +8,7 @@ void genRandArray(dot* arr, int size)
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis(0, 10);
+	std::uniform_int_distribution<> dis(-10000, 10000);
 	for (int i = 0; i < size; i++)
 	{
 		arr[i].x = dis(gen);
@@ -16,19 +16,71 @@ void genRandArray(dot* arr, int size)
 	}
 }
 
+std::pair<dot*, int> grehemMethod(dot* dotArray, int size)
+{
+	searchMinElement(dotArray, size - 1, 8);
+	dot move = dotArray[0];
+	for (auto i = 0; i < size - 1; i++)
+		dotArray[i] = dotArray[i] - move;
+	quickSort(dotArray, 1, size - 1);
+	dotArray[size - 1] = dotArray[0];
+	stack dotStack(size / 2);
+	dot dotY, dotX;
+	dotStack.push(dotArray[0]);
+	dotStack.push(dotArray[1]);
+	for (auto i = 2; i < size; i++)
+	{
+		while (true)
+		{
+			if (dotStack.getSize() > 1)
+			{
+				dotY = dotStack.pop();
+				dotX = dotStack.get();
+				if ((dotArray[i].x*(dotX.y - dotY.y) + dotArray[i].y*(dotY.x - dotX.x) + dotX.x*dotY.y - dotX.y*dotY.x) > 0)
+				{
+					dotStack.push(dotY);
+					dotStack.push(dotArray[i]);
+					break;
+				}
+				else
+				{
+					if (dotStack.getSize() == 1)
+					{
+						dotStack.push(dotArray[i]);
+						break;
+					}
+				}
+			}
+		}
+	}
+	dot* resultArray = dotStack.getArray();
+	for (auto i = 0; i < dotStack.getSize(); i++)
+	{
+		resultArray[i] = resultArray[i] + move;
+	}
+	return std::make_pair(resultArray, dotStack.getSize());
+}
+
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int size = 20;
+	int size = 40;
 	dot* arr = new dot[size + 1];
 	genRandArray(arr, size);
 	int num_threads = 8;
+	omp_set_num_threads(num_threads);
 
 	std::cout << "Сгенерированный массив" << std::endl;
+
+	dot* arr2 = new dot[size + 1];//
+
 	for (int i = 0; i < size; i++)
 	{
 		std::cout << arr[i].x << " " << arr[i].y << std::endl;
+		arr2[i] = arr[i];//
 	}
+
 
 	auto start = std::chrono::high_resolution_clock::now();
 	std::pair<dot*, int> answer = grehemMethod_OpenMP(arr, size + 1, num_threads);
@@ -40,6 +92,20 @@ int main()
 	{
 		std::cout << answer.first[i].x << " " << answer.first[i].y << std::endl;
 	}
+
+	start = std::chrono::high_resolution_clock::now();
+	std::pair<dot*, int> answer2 = grehemMethod(arr2, size + 1);
+	end = std::chrono::high_resolution_clock::now();
+	std::cout << " Время выполнения " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " (микросекунды)" << std::endl;
+
+	std::cout << "Выпуклая оболочка" << std::endl;
+	for (int i = 0; i < answer2.second; i++)
+	{
+		std::cout << answer2.first[i].x << " " << answer2.first[i].y << std::endl;
+	}
+
+	if (answer.second != answer2.second)
+		std::cout << "ACHTUNG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
 
 	return 0;
 }
