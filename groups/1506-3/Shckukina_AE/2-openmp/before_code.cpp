9bbeac47f66441a62703665ff8ea86f4923a1272
Аня <anya15-98@mail.ru>
2018-04-20 00:31:59 +0300

    changes for omp

--- groups/1506-3/Shckukina_AE/2-openmp/before_code.cpp
+++ groups/1506-3/Shckukina_AE/2-openmp/before_code.cpp
@@ -1,45 +1,89 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstdio> 
-#include <random>
+
 #include <omp.h>
-
-
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+#include <vector>
+#include<iterator>
+#include <algorithm>
 using namespace std;
 
-void quicksort(double* arr, int size);
-void quicksort_omp(double arr[], int first, int last);
+void QuickSort(double* arr, int size);
+int Bin_Search_ind(const std::vector<double> arr, int left, int right, double key);//бинарный поиск максимального элемента слева относительно key
+void ShareAndMerge(std::vector<double> vec1, std::vector<double> vec2, double* write_to);
 
 int main(int argc, char * argv[])
 {
-	if (argc != 3) {
-		std::cout << "Error! You should input 'solver input.bin output.bin'" << std::endl;
+	if (argc != 4) {
+		std::cout << "Use 'omp_qs [threads] [input file] [output file]'" << std::endl;
 		return 1;
 	}
 
-	int size;
-	double* array;
-	double _time;
+	int size, threads = atoi(argv[1]);
+	double* arr;
+	double fict_time;
 
-
-	freopen(argv[1], "rb", stdin);
-	freopen(argv[2], "wb", stdout);
-
-	fread(&_time, sizeof(_time), 1, stdin);
+	freopen(argv[2], "rb", stdin);
+	fread(&fict_time, sizeof(fict_time), 1, stdin);
 	fread(&size, sizeof(size), 1, stdin);
 
-	array = new double[size];
-	fread(array, sizeof(*array), size, stdin);
+	arr = new double[size];
+
+	fread(arr, sizeof(*arr), size, stdin);
 
 	double time = omp_get_wtime();
-	//quicksort(array, size);
-	quicksort_omp(array, 0, size - 1);
+	int step;
+	std::vector<double>* tempArray = new std::vector<double>[threads]; //массив 4х векторов 
+	int *shift = new int[threads], *chunk = new int[threads]; //сдвиг относительно начала, хвост относительно сдвига
+#pragma omp parallel shared(arr, step, shift, chunk, tempArray) num_threads(threads) 
+	{
+		int tid, thread_index; //ид текущего потока 
+		tid = omp_get_thread_num();
+
+		shift[tid] = tid * (size / threads); //начало с которого будем сортировать
+
+		chunk[tid] = (tid == threads - 1) ? size - tid * (size / threads) : size / threads;
+
+		QuickSort(arr + shift[tid], chunk[tid]);
+
+#pragma omp barrier //ждешь все потоки
+
+		step = 1; //шаг рекурсии
+		while (step < threads) {
+			thread_index = (int)pow(2, step - 1); //позволяет найти следующиий поток
+
+			for (int i = 0; i < chunk[tid]; ++i) { //записываю каждый кусох исходного массива в нужный вектор
+				tempArray[tid].push_back(*(arr + shift[tid] + i));
+			}
+
+#pragma omp barrier //ждем все потоки
+			if (tid % (thread_index * 2) == 0) { //в каждой паре берем левый 
+				ShareAndMerge(tempArray[tid], tempArray[tid + thread_index], &arr[shift[tid]]);
+				chunk[tid] += chunk[tid + thread_index];
+				tempArray[tid].clear(); tempArray[tid].shrink_to_fit(); //удаление памяти
+				tempArray[tid + thread_index].clear(); tempArray[tid + thread_index].shrink_to_fit();
+			}
+#pragma omp single 
+			{
+				step *= 2;
+			}
+#pragma omp barrier
+		}
+	}
+	delete[] tempArray;
+	delete[] chunk;
+	delete[] shift;
+
 	time = omp_get_wtime() - time;
 
+	printf("OpenMP time: %f\n", time);
+
+	freopen(argv[3], "wb", stdout);
 	fwrite(&time, sizeof(time), 1, stdout);
 	fwrite(&size, sizeof(size), 1, stdout);
-
-	fwrite(array, sizeof(*array), size, stdout);
-
+	fwrite(arr, sizeof(*arr), size, stdout);
+	system("pause");
 	return 0;
+
 }
