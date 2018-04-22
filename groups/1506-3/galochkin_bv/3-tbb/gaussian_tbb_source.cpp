a09e95d865455b83e31520702c766d78f8452ebf
drinkertea <spore17@mail.ru>
2018-04-22 19:17:02 +0300

    upd code, for data safe

--- groups/1506-3/galochkin_bv/3-tbb/gaussian_tbb_source.cpp
+++ groups/1506-3/galochkin_bv/3-tbb/gaussian_tbb_source.cpp
@@ -4,14 +4,15 @@
 #include <iostream>
 #include "tbb/tbb.h"
 #include "tbb/tick_count.h"
-#define PI 3.14159265358979323846
 
+#define PI 3.14159265358979323846
+using data_vec = std::vector<double>;
 typedef struct {
-	double value[3]{ 0,0,0 };
-	double weight[3]{ 0,0,0 };
+	double value[3];
+	double weight[3];
 } Weights; //struct for filter algorithm
 
-double* gaussian_blur_parallelTBB(double* image, int h, int w, double r, int bsize);
+void gaussian_blur_parallelTBB(const data_vec& image, data_vec& result, int h, int w, double r);
 
 int border(int val, int min, int max, int r = 0) {
 	if (val < min) return min + r; //when out of border - mirror pixels
@@ -19,28 +20,28 @@ int border(int val, int min, int max, int r = 0) {
 	return val;
 }
 
-double* read_binary_image_file(int&w, int&h, const std::string& dir = "image.in") {
+data_vec read_binary_image_file(int&w, int&h, const std::string& dir = "image.in") {
 	freopen(dir.c_str(), "rb", stdin);
 	fread(&w, sizeof(w), 1, stdin);
 	fread(&h, sizeof(h), 1, stdin);
-	double *data = new double[h * w * 3];
-	fread(data, sizeof(*data), h * w * 3, stdin);
+    data_vec data(h * w * 3, 0.0);
+	fread(reinterpret_cast<void*>(&data[0]), sizeof(data[0]), h * w * 3, stdin);
 	return data;
 }
 
-void save_image_to_binary(int&w, int&h, double *data, double& t, const std::string& dir = "image.out")
+void save_image_to_binary(int&w, int&h, data_vec& data, double& t, const std::string& dir = "image.out")
 {
 	freopen(dir.c_str(), "wb", stdout);
 	fwrite(&w, sizeof(w), 1, stdout);
 	fwrite(&h, sizeof(h), 1, stdout);
 	fwrite(&t, sizeof(t), 1, stdout);
-	fwrite(data, sizeof(*data), h * w * 3, stdout);
+	fwrite(reinterpret_cast<void*>(&data[0]), sizeof(data[0]), h * w * 3, stdout);
 }
 
 int main(int argc, const char** argv)
 {
 	int w, h; //image size
-	double *data; //pixels by lines R,G,B,R,G,B,R...
+    data_vec data; //pixels by lines R,G,B,R,G,B,R...
 	int threads_num = argc > 1 ? std::stoi(argv[1]) : 4; //count of threads
 	int radius = argc > 2 ? std::stoi(argv[2]) : 1; //filter radius 1 by default (3x3)
 	std::string input_file = argc > 3 ? std::string(argv[3]) : "image.in"; //file dir
@@ -48,25 +49,22 @@ int main(int argc, const char** argv)
     tbb::task_scheduler_init init(threads_num);
 
 	data = read_binary_image_file(w, h, input_file);
+    data_vec blured(h * w * 3, 0.0);
 
 	auto parallel_time = tbb::tick_count::now();
-    int block_size = h / (threads_num * 4);
-	double* blured = gaussian_blur_parallelTBB(data, h, w, radius, block_size);
+    gaussian_blur_parallelTBB(data, blured, h, w, radius);
     double p_time = (tbb::tick_count::now() - parallel_time).seconds();
 	//printf("Parallel time: %lfs\n", p_time);
     save_image_to_binary(w, h, blured, p_time, output_file);
 
-	delete[] blured;
-	delete[] data;
 	return 0;
 }
 
-double* gaussian_blur_parallelTBB(double* image, int h, int w, double r, int bsize) {	
-	double* res = new double[h * w * 3];
-    tbb::parallel_for(tbb::blocked_range<int>(0, h, bsize), [image, res, &h, &w, &r](const tbb::blocked_range<int>& heigth) {
+void gaussian_blur_parallelTBB(const data_vec& image, data_vec& result, int h, int w, double r) {
+    tbb::parallel_for(tbb::blocked_range<int>(0, h), [&image, &result, &h, &w, &r](const tbb::blocked_range<int>& heigth) {
         for (int i = heigth.begin(); i != heigth.end(); ++i) {
             for (int j = 0; j < w; j++) {
-                Weights weights;
+                Weights weights = {};
                 for (int iy = i - r; iy < i + r + 1; iy++) {
                     for (int ix = j - r; ix < j + r + 1; ix++) {
 
@@ -81,12 +79,11 @@ double* gaussian_blur_parallelTBB(double* image, int h, int w, double r, int bsi
                     }
                 }
                 for (int c = 0; c < 3; c++) {
-                    res[w * i * 3 + j * 3 + c] = round(weights.value[c] / weights.weight[c]);
+                    result[w * i * 3 + j * 3 + c] = round(weights.value[c] / weights.weight[c]);
                     weights.value[c] = 0.0;
                     weights.weight[c] = 0.0;
                 }
             }
         }
     });
-	return res;
 }
