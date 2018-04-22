0e52b2457dab628b5b8c8fc715920ec9aea77162
Azarius <osh1308@gmail.com>
2018-04-22 11:57:18 +0300

    add OpenMP version

---
+++ groups/1506-3/lipatov_id/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf "compiling generator\n"
+g++ -O3 ./Test/generator.cpp -o ./Test/gen
+printf "compiling typer\n"
+g++ -O3 ./Test/typer.cpp -o ./Test/t
+printf "compiling main program\n"
+g++ -O3 ./OpenMP/before_code.cpp -fopenmp -o ./OpenMP/b
+printf "compiling viewer\n"
+g++ -O3 ./Test/viewer.cpp -o ./Test/v
+printf "done\n"
--- groups/1506-3/lipatov_id/run.sh
+++ groups/1506-3/lipatov_id/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-g++ generator.cpp -o gen
-g++ typer.cpp -o t
-g++ before_code.cpp -o b
-g++ viewer.cpp -o v
-./gen 9 t_in
-./t t_in.txt t_out.bin
-./b 2 t_out.bin b_out.bin
-./v b_out.bin
+for file in ./Tests/*; do
+  if [[ "$file" != *perfect.ans ]];
+  then
+    ./OpenMP/b 4 $file seq_$file parr_$file
+  fi
+done
