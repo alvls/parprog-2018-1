9d4f155364e1f627d1a6cad0f4d7764e62998753
Lvova Alina <37267494+AlinaLvova@users.noreply.github.com>
2018-05-25 04:13:25 +0300

    Update sol.cpp

--- groups/1506-3/lvova_ad/2-openmp/sol.cpp
+++ groups/1506-3/lvova_ad/2-openmp/sol.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 
 
-void InsertSort(double* arr, int i, int length, int half) {
+void insert_sort(double* arr, int i, int length, int half) {
     
     double temp = 0;
     int j = 0;
@@ -19,16 +19,28 @@ void InsertSort(double* arr, int i, int length, int half) {
     }
 }
 
-void shellSortParallel(double* array, int length)
+void shell_sort_with_a_shift(double* array, int shift, int length)
 {
     int h;
     int i = 0;
     for (h = length / 2; h > 0; h = h / 2)
     {
-#pragma omp parallel for 
         for (i = 0; i < h; i++)
         { 
-            InsertSort(array, i, length, h);
+            insert_sort(array, i + shift, length + shift, h);
+        }
+    }
+
+}
+void shell_sort(double* array, int length)
+{
+    int h;
+    int i = 0;
+    for (h = length / 2; h > 0; h = h / 2)
+    {
+        for (i = 0; i < h; i++)
+        {
+            insert_sort(array, i, length, h);
         }
     }
 }
