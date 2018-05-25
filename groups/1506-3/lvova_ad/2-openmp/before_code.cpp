e08da7558b07561f955ff58b7083be43acbaa93d
Lvova Alina <37267494+AlinaLvova@users.noreply.github.com>
2018-05-25 04:11:38 +0300

    Update before_code.cpp

--- groups/1506-3/lvova_ad/2-openmp/before_code.cpp
+++ groups/1506-3/lvova_ad/2-openmp/before_code.cpp
@@ -4,7 +4,11 @@
 #include <random>
 #include <iostream>
 
-void shellSortParallel(double * A, int size);
+void shell_sort_with_a_shift(double* array, int iteration, int length);
+
+
+//void shell_parallel_tbb_sort(double* array, int length, int n_threads);
+void shell_parallel_openmp_sort(double* array, int length, int n_threads);
 
 int main(int argc, char * argv[])
 {
@@ -20,18 +24,30 @@ int main(int argc, char * argv[])
 	A = new double[size];
 
 	fread(A, sizeof(*A), size, input_file);
-
-	omp_set_num_threads(num_threads);
-
-    if (size <= 100)
+   
+     if (size <= 100) {
+        std::cout << std::endl;
         for (int i = 0; i < size; i++)
             std::cout << A[i] << " ";
+    }
 
 	double time = omp_get_wtime();
-
-    shellSortParallel(A, size);
+    if (num_threads > 1)
+        shell_parallel_openmp_sort(A, size, num_threads);
+    else
+        shell_sort_with_a_shift(A, 0, size);
 
 	time = omp_get_wtime() - time;
+
+    if (size <= 100)
+    {
+        std::cout << std::endl << std::endl;
+        for (int i = 0; i < size; i++)
+            std::cout << A[i] << " ";
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl << "time = " << time << std::endl;
 
 	fwrite(&time, sizeof(time), 1, output_file);
 	fwrite(&size, sizeof(size), 1, output_file);
