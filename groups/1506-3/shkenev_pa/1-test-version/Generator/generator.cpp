0c73086b608c7791b1cedbb2a173f1b2bcbd5315
PetrShkenev <potc.coas@live.ru>
2018-06-28 17:43:47 +0300

    Generator fix

--- groups/1506-3/shkenev_pa/1-test-version/Generator/generator.cpp
+++ groups/1506-3/shkenev_pa/1-test-version/Generator/generator.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <cstdlib>
+#include <ctime>
 
 // generator выходной_файл ширина высота
 int main(int argc, char *argv[])
@@ -8,15 +9,28 @@ int main(int argc, char *argv[])
 	int height = atoi(argv[3]); // высота изображения
 
 	int imageLength = width * height * 3;
-	unsigned char *image = new unsigned char[];
-	image[0] = image[1] = image[2] = rand();
-	for (int i = 1; i < width; i++)
+	unsigned char *image = new unsigned char[imageLength];
+
+    srand((int)time(NULL));
+    unsigned char colourR = rand();
+    unsigned char colourG = rand();
+    unsigned char colourB = rand();
+	for (int j = 0; j < imageLength; j += width * 3)
 	{
-		for (int j = 1; j < height; j++)
-		{
-			image[width * j + i] = rand();
-		}
-	}
+        for (int i = 0; i < width; i++)
+        {
+            image[j + i * 3] = colourR;
+            image[j + i * 3 + 1] = colourG;
+            image[j + i * 3 + 2] = colourB;
+        }
+
+        colourR += rand() % 7 - 3;
+        colourG += rand() % 7 - 3;
+        colourB += rand() % 7 - 3;
+    }
+
+    for (int i = 0; i < imageLength; i += rand() % 50)
+        image[i] = rand();
 
 	std::ofstream ofs(argv[1], std::ios::binary);
 	ofs.write((char *)&width, sizeof(width));
